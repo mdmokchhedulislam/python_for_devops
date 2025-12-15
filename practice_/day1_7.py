ab7a295101584999f64a156445dc6be5e0d93157
mokchhedulislam <mokchheduls46@gmail.com>
2025-12-15 16:26:05 +0600

    check prime number and create reverse function

---
+++ practice_/day1_7.py
@@ -0,0 +1,19 @@
+# reverse
+a= "hellow world"
+
+def reve_str(s):
+    return s[::-1]
+
+
+# print(reve_str(a))
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(36))
