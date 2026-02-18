bfe1a9daa4ebf1c786afaf892efe083d9c48fbef
mokchhedulislam <mokchheduls46@gmail.com>
2026-02-18 09:32:10 +0600

    add automation 2 container check and start

---
+++ automation/100automation/automation2.py
@@ -0,0 +1,12 @@
+# check and start container
+
+import subprocess
+def check_start_docker():
+    running_containers=subprocess.check_output(["docker", "ps"]).decode('utf-8')
+    if "ecom_app" not in running_containers:
+        print("app is not running , need to start")
+        subprocess.run(['docker', 'start','ecom_app'])
+    else:
+        print("container is ok")
+
+check_start_docker()
